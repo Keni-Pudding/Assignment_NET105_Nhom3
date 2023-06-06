380d423e3fadb993ff0a71352067718f9eef6a3f
Nguyễn Văn Thắng <thangnguyen1632003@gmail.com>
2023-06-06 21:55:36 +0700

    á

--- Assignment_NET105_Nhom3_Shared/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Assignment_NET105_Nhom3_Shared
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Assignment_NET105_Nhom3_Shared/Models/CartDetails.cs
@@ -0,0 +1,14 @@
+﻿namespace Assignment_NET105_Nhom3.Models
+{
+    public class CartDetails
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid? ProductDetailId { get; set; }
+        public Guid? ComboId { get; set; }
+        public int Quantity { get; set; }
+        public virtual Cart Cart { get; set; }
+        public virtual ProductDetails ProductDetails { get; set; }
+        public virtual Combos Combos { get; set; }
+    }
+}
