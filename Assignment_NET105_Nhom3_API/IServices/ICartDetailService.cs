bcb9da81bbcd6e73c79999dbb9b944fb2f33bd86
duythai010305@gmail.com <duythai010305@gmail.com>
2023-06-09 00:29:39 +0700

    thaideptraivancodeapi

---
+++ Assignment_NET105_Nhom3_API/IServices/ICartDetailService.cs
@@ -0,0 +1,14 @@
+﻿using Assignment_NET105_Nhom3.Models;
+
+namespace Assignment_NET105_Nhom3_API.IServices
+{
+    public interface ICartDetailService
+    {
+        public List<CartDetails> GetAllCartDetailsDetails();
+        public Task<List<CartDetails>> GetAllCartDetailssAsync();
+        public Task<CartDetails> GetCartDetailssByIDAsync(Guid ID);
+        public Task<CartDetails> PostCartDetailssAsync(CartDetails CartDetails);
+        public Task<CartDetails> PutCartDetailssAsync(CartDetails CartDetails);
+        public Task<CartDetails> DeleteCartDetailssAsync(Guid ID);
+    }
+}
