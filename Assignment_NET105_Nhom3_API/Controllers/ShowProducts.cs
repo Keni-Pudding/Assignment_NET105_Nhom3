f2d8469ee2310cea41ba8a29ac4bc239f52600cb
duythai010305@gmail.com <duythai010305@gmail.com>
2023-06-08 23:48:42 +0700

    thaideptrailamxongapi

---
+++ Assignment_NET105_Nhom3_API/Controllers/ShowProducts.cs
@@ -0,0 +1,25 @@
+﻿using Assignment_NET105_Nhom3.Models;
+using Assignment_NET105_Nhom3_API.IServices;
+using Assignment_NET105_Nhom3_API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment_NET105_Nhom3_API.Controllers
+{
+    [Route("api/ShowProduct11")]
+    [ApiController]
+    public class ShowProducts : ControllerBase
+    {
+        private readonly IShowProductsService _showProductServices;
+        public ShowProducts(IShowProductsService _a)
+        {
+            _showProductServices = _a;
+        }
+        [HttpGet]
+        public async Task<ActionResult<Products>> GetAllProducts() //  Lấy danh sách data
+        {
+            var lpd = await _showProductServices.GetAllProductAsync();
+            return Ok(lpd);
+        }
+    }
+}
---
+++ Assignment_NET105_Nhom3_API/IServices/IShowProductsService.cs
@@ -0,0 +1,9 @@
+﻿using Assignment_NET105_Nhom3.Models;
+
+namespace Assignment_NET105_Nhom3_API.IServices
+{
+    public interface IShowProductsService
+    {
+        public Task<List<Products>> GetAllProductAsync();
+    }
+}
